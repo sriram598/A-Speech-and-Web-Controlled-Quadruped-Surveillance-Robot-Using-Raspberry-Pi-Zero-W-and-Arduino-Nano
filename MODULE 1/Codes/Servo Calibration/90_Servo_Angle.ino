601f5a1eb909f54af0d3ecb6cd7a8e8502fc86ae
Sriram <tungalasriram@gmail.com>
2026-09-17 09:45:41 +0530

    Update 90_Servo_Angle.ino

--- MODULE 1/Codes/Servo Calibration/90_Servo_Angle.ino
+++ MODULE 1/Codes/Servo Calibration/90_Servo_Angle.ino
@@ -1,1 +1,33 @@
+//2300049817//
 
+#include <Servo.h>   
+
+Servo servo[4][3];
+
+//define servos' ports
+const int servo_pin[4][3] = { {3, 4, 2}, {6, 7, 5}, {9, 8, 10},{12, 11, 13} };
+
+void setup()
+{
+  //initialize all servos
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      servo[i][j].attach(servo_pin[i][j]);
+      delay(20);
+    }
+  }
+}
+
+void loop(void)
+{
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      servo[i][j].write(90);
+      delay(20);
+    }
+  }
+}
